ff097257d9719100a4fd0a63363fa5a0d082fbdb
Manindra Gautam <manindra.gautam@ardentisys.com>
2021-08-06 02:00:36 +0530

    final changes

--- packages/app2/src/App.js
+++ packages/app2/src/App.js
@@ -1,99 +1,13 @@
-import { Suspense, lazy, useState } from "react";
+import { Suspense, lazy } from "react";
+import Todo from "./Todo";
 
-const App1 = lazy(() => import('app1/App'));
-
-function Todo({ todo, index, completeTodo, removeTodo }) {
-    return (
-        <div
-            className="todo"
-            style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-        >
-            {todo.text}
-            <div>
-                {!todo.isCompleted && <button onClick={() => completeTodo(index)}>Complete</button>}
-                <button onClick={() => removeTodo(index)}>x</button>
-            </div>
-        </div>
-    );
-}
-
-function TodoForm({ addTodo }) {
-    const [value, setValue] = useState("");
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (!value) return;
-        addTodo(value);
-        setValue("");
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                className="input"
-                value={value}
-                onChange={e => setValue(e.target.value)}
-            />
-        </form>
-    );
-}
-
-function TodoList() {
-    const [todos, setTodos] = useState([
-        {
-            text: "Learn about React",
-            isCompleted: false
-        },
-        {
-            text: "Meet friend for lunch",
-            isCompleted: false
-        },
-        {
-            text: "Build really cool todo app",
-            isCompleted: false
-        }
-    ]);
-
-    const addTodo = text => {
-        const newTodos = [...todos, { text }];
-        setTodos(newTodos);
-    };
-
-    const completeTodo = index => {
-        const newTodos = [...todos];
-        newTodos[index].isCompleted = true;
-        setTodos(newTodos);
-    };
-
-    const removeTodo = index => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
-    };
-
-    return (
-        <div className="todo-list">
-            <h1>Todo App</h1>
-            {todos.map((todo, index) => (
-                <Todo
-                    key={index}
-                    index={index}
-                    todo={todo}
-                    completeTodo={completeTodo}
-                    removeTodo={removeTodo}
-                />
-            ))}
-            <TodoForm addTodo={addTodo} />
-        </div>
-    );
-}
+const Counter = lazy(() => import('app1/App'));
 
 export default function App() {
-    return <div className="grid grid-cols-2 divide-x divide-gray-500">
+    return <div className="grid grid-cols-2">
         <Suspense fallback={<div>Loading...</div>}>
-            <App1 />
+            <Counter />
         </Suspense>
-        <TodoList />
+        <Todo />
     </div>
 }
